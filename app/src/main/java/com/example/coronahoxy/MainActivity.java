e68d37db1591d7c4ffbd42e29009f7428ac0a8c1
SungChulByun <spide333@kaist.ac.kr>
2020-02-26 23:35:59 +0900

    라이브러리 정리

--- app/src/main/java/com/example/coronahoxy/MainActivity.java
+++ app/src/main/java/com/example/coronahoxy/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.coronahoxy;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this,
+            Manifest.permission.INTERNET)
+            != PackageManager.PERMISSION_GRANTED) {
+
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.READ_CONTACTS)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+            } else {
+                // No explanation needed; request the permission
+                ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_CONTACTS},
+                    1000);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        } else {
+            // Permission has already been granted
+        }
+
     }
 }
